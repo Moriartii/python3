5199454a46e8aee60b5069c7d42c97e64853e270
Nikolay <mzdnik0@gmail.com>
2020-10-22 04:01:58 +0300

    test commit 3

--- hello_world.py
+++ hello_world.py
@@ -3,13 +3,3 @@ import time
 for x in range(10):
     print("Hello World!")
     time.sleep(0.2)
-
-import this
-
-for x in range(10):
-    print("Hello World!")
-    time.sleep(0.2)
-
-check = "check"
-print("")
-print(f'who {check} this?')
